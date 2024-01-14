b2e013962c9be9f5d55f6e60aae6ca7e3088a3d0
Maryam Mekraz <m.mekraz@uot.edu.ly>
2024-01-15 01:10:49 +0200

    Finished.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:maharah_project/home_page/home_header.dart';
+import 'package:maharah_project/home_page/home_screen.dart';
 import 'package:maharah_project/user_profile/user_profile_screen.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: UserProfileScreen(),
+      home: HomeScreen(),
     ),
   );
 }
